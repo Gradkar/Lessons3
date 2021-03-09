007ab85f0e0093dd3652c2d83c9dfc979d1ebd83
Zdanovich Stanislav <80322577+Gradkar@users.noreply.github.com>
2021-03-09 15:08:37 +0300

    Add files via upload

---
+++ Prime number.py
@@ -0,0 +1,25 @@
+import math
+print("Enter the number you would like to check")
+a = int(input())
+while(True):
+    if a % math.sqrt(a) == 0:
+        print("The number is not simple.")
+        break
+    if a % 2 == 0:
+        print("The number is not simple. Divided into 2")
+        break
+    else:
+        if a % 3 == 0:
+            print("The number is not simple. Divided into 3")
+            break
+        else:  
+            if a % 5 == 0:
+                print("The number is not simple. Divided into 5")
+                break
+            else:
+                if a % 7 == 0:
+                    print("The number is not simple. Divided into 7")
+                    break
+                else:
+                    print("Prime number")
+                    break

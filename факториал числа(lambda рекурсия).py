ecdecd96fabf3b81edb6d766f70b16f77c75fbff
Zdanovich Stanislav <80322577+Gradkar@users.noreply.github.com>
2021-03-18 13:13:04 +0300

    Delete факториал числа(lambda рекурсия).py

--- факториал числа(lambda рекурсия).py
+++
@@ -1,8 +0,0 @@
-factorial = lambda x: factorial(x - 1) * x if x > 1 else 1
-print("Введите число, факториал которого будем считать")
-a = input()
-if (a.isdigit()):
-    a = int(a)
-    print("Факториал числа равен",factorial(a))
-else:
-    print("Попробуйте ввести число еще раз")
